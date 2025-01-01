7410b5066f8e6d8e8dc6e02cd6c7e0678b35cf25
jaewoo0530 <6672jjw@gmail.com>
2025-01-01 19:25:45 +0900

    [ADD]: Jump

--- Assets/Scenes/Scripts/PlayerController.cs
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 3f;   // 최대 속도
+    public float jumpForce = 4f;
     private Rigidbody2D playerRigidbody2D;
+    private bool isGrounded;
 
     private void Awake()
     {
@@ -15,6 +17,7 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         Move();
+        Jump();
     }
 
     private void Move()
@@ -22,5 +25,36 @@ public class PlayerController : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal");
         Vector2 move = new Vector2(xInput * speed, playerRigidbody2D.velocity.y);
         playerRigidbody2D.velocity = move;
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            playerRigidbody2D.velocity = new Vector2(playerRigidbody2D.velocity.x, jumpForce);
+        }
+        if (Input.GetKeyUp(KeyCode.Space) && playerRigidbody2D.velocity.y > 0)
+        {
+            playerRigidbody2D.velocity = new Vector2(playerRigidbody2D.velocity.x, playerRigidbody2D.velocity.y * 0.5f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts[0].normal.y > 0.7f)
+        {
+            if (collision.gameObject.CompareTag("Ground"))
+            {
+                isGrounded = true;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
