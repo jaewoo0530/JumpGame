7d419abe9fd7b16a1eaedbe4fdef106671b68300
jaewoo0530 <6672jjw@gmail.com>
2025-01-04 22:05:51 +0900

    [ADD]: OnemoreJump script

---
+++ Assets/Scripts/OnemoreJump.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnemoreJump : MonoBehaviour, IItem
+{
+    public void Use(GameObject target)
+    {
+
+    }
+}
+
