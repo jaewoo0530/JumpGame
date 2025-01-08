6f4524ac63857e701e2cd7e94301b5b705818112
jaewoo0530 <6672jjw@gmail.com>
2025-01-08 19:34:00 +0900

    [ADD]: CheckPoint scripts

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // 플레이어가 체크포인트를 통과했는지 확인
+        if (other.CompareTag("Player"))
+        {
+            // GameManager의 respawnPoint를 현재 체크포인트 위치로 갱신
+            GameManager.Instance.respawnPoint = transform;
+            Debug.Log("Checkpoint Updated to: " + transform.position);
+        }
+    }
+}
