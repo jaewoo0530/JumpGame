ca53562543a3cd847f103c2f9c985abac232deb6
jaewoo0530 <6672jjw@gmail.com>
2025-01-09 22:45:35 +0900

    [ADD]: checkpoint animator code

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
+    private Animator checkpointAnimator;
+
+    private void Awake()
+    {
+        checkpointAnimator = GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         // 플레이어가 체크포인트를 통과했는지 확인
@@ -10,6 +17,13 @@ public class CheckPoint : MonoBehaviour
             // GameManager의 respawnPoint를 현재 체크포인트 위치로 갱신
             GameManager.Instance.respawnPoint = transform;
             Debug.Log("Checkpoint Updated to: " + transform.position);
+            checkpointAnimator.SetBool("Activate", true);
         }
+    }
+
+    private void Idle()
+    {
+        checkpointAnimator.SetBool("Idle", true);
+        checkpointAnimator.SetBool("Activate", false);
     }
 }
