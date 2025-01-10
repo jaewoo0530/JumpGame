694484afbf3bb7b6dbce2d218ba063209c9033a7
jaewoo0530 <6672jjw@gmail.com>
2025-01-11 02:29:39 +0900

    [Fix]: random checkpoint

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -13,7 +13,9 @@ public class CheckPoint : MonoBehaviour
     {
         GameManager.Instance.respawnPoint = transform;
         Debug.Log("Checkpoint Updated to: " + transform.position);
+        checkpointAnimator.SetBool("Idle", false);
         checkpointAnimator.SetBool("Activate", true);
+        checkpointAnimator.SetBool("Deactivate", false);
     }
 
 
@@ -21,11 +23,13 @@ public class CheckPoint : MonoBehaviour
     {
         checkpointAnimator.SetBool("Idle", true);
         checkpointAnimator.SetBool("Activate", false);
+        checkpointAnimator.SetBool("Deactivate", false);
     }
 
     public void Deactivate()
     {
         // 체크포인트 비활성화 애니메이션
+        checkpointAnimator.SetBool("Idle", false);
         checkpointAnimator.SetBool("Activate", false);
         checkpointAnimator.SetBool("Deactivate", true); // 비활성화 애니메이션
     }
